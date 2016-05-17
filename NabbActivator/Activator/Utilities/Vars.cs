6aa31ea59d5281271c24413f4cdb25634dbe73e2
Exory <drago.enrico@hotmail.it>
2016-05-17 12:52:20 +0200

    [SDK]NabbActivator: 6.9.0.4
    Fixed Cassiopeia's W

--- NabbActivator/Activator/Utilities/Vars.cs
+++ NabbActivator/Activator/Utilities/Vars.cs
@@ -13,7 +13,7 @@ namespace NabbActivator
         /// <summary>
         ///     A list of the names of the champions who cast Invalid Snares.
         /// </summary>
-        public static readonly List<string> InvalidSnareCasters = new List<string> {"Leona", "Zyra", "Lissandra"};
+        public static readonly List<string> InvalidSnareCasters = new List<string> {"Leona", "Zyra", "Lissandra", "Cassiopeia"};
 
         /// <summary>
         ///     A list of the names of the champions who cast Invalid Stuns.
