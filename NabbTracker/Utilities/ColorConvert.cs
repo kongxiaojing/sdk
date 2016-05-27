4b0bd504a5f5e44ea24b5a1fbc59646809e82c05
Exory <drago.enrico@hotmail.it>
2016-05-27 02:13:58 +0200

    [SDK]NabbTracker: 6.10.1.3
    Solved a little color bug.

--- NabbTracker/Utilities/ColorConvert.cs
+++ NabbTracker/Utilities/ColorConvert.cs
@@ -14,6 +14,9 @@ namespace NabbTracker
 	/// </summary>
 	internal class Colors
 	{
+		/// <summary>
+		///     The SharpDX.Color type table.
+		/// </summary>
 		public static int SDXColor(SharpDX.Color color)
 		{
 			if (color == SharpDX.Color.Gray)       return 0;
@@ -26,6 +29,9 @@ namespace NabbTracker
 			return -1;
 		}
 
+		/// <summary>
+		///     The System.Drawing.Color type table.
+		/// </summary>
 		public static int SDColor(System.Drawing.Color color)
 		{
 			if (color == System.Drawing.Color.Gray)       return 0;
@@ -38,16 +44,20 @@ namespace NabbTracker
 			return -1;
 		}
 
+		/// <summary>
+		///		Converts a SharpDX.Color type into the same color as seen by a colorblind person
+		///		for each type of colorblindness, using the 'FromBgra' function.
+		/// </summary>
 		public static SharpDX.Color Convert(SharpDX.Color color)
 		{
 			var sdxConvert = new uint[6, 5] 
 			{
-				{ 0xFF7F7F81, 0xFF807E82, 0xFF808080, 0xFF7F7F7F, 0xFF808080 }, // gray
-				{ 0xFFFFE802, 0xFFFFF205, 0xFFFFD6D0, 0xFFE1E1E1, 0xFFFFFF00 }, // yellow
-				{ 0xFF6D5600, 0xFF332802, 0xFFF70500, 0xFF4C4C4C, 0xFFFF0000 }, // red
-				{ 0xFF91A8FF, 0xFFCCD3FF, 0xFF07F9FF, 0xFFB2B2B2, 0xFF00FFFF }, // cyan
-				{ 0xFFD3C594, 0xFFEDD893, 0xFF9ADCE2, 0xFFC7C7C7, 0xFF90EE90 }, // lightgreen
-				{ 0xFF23377D, 0xFF011981, 0xFF71170F, 0xFF343434, 0xFF800080 }  // purple
+				{ 0xFF808080, 0xFF7F7F81, 0xFF807E82, 0xFF808080, 0xFF7F7F7F }, // gray
+				{ 0xFFFFFF00, 0xFFFFE802, 0xFFFFF205, 0xFFFFD6D0, 0xFFE1E1E1 }, // yellow
+				{ 0xFFFF0000, 0xFF6D5600, 0xFF332802, 0xFFF70500, 0xFF4C4C4C }, // red
+				{ 0xFF00FFFF, 0xFF91A8FF, 0xFFCCD3FF, 0xFF07F9FF, 0xFFB2B2B2 }, // cyan
+				{ 0xFF90EE90, 0xFFD3C594, 0xFFEDD893, 0xFF9ADCE2, 0xFFC7C7C7 }, // lightgreen
+				{ 0xFF800080, 0xFF23377D, 0xFF011981, 0xFF71170F, 0xFF343434 }  // purple
 			};
 
 			Vars.SDXColor = 
@@ -59,16 +69,20 @@ namespace NabbTracker
 			return Vars.SDXColor;
 		}
 
+		/// <summary>
+		///		Converts a System.Drawing.Color type into the same color as seen by a colorblind person
+		///		for each type of colorblindness, using the 'FromArgb' function.
+		/// </summary>
 		public static System.Drawing.Color Convert(System.Drawing.Color color)
 		{
 			var sdConvert = new int[6, 5, 3]
 			{
-				{ { 127, 127, 129 }, { 128, 126, 130 }, { 128, 128, 128 }, { 127, 127, 127 }, { 128, 128, 128 } }, // gray
-				{ { 255, 232, 2 },   { 255, 242, 5 },   { 255, 214, 208 }, { 225, 225, 225 }, { 255, 255, 0 }   }, // yellow
-				{ { 109, 86, 0 },    { 51, 40, 2 },     { 247, 5, 0 },     { 76, 76, 76 },    { 255, 0, 0 }     }, // red
-				{ { 145, 168, 255 }, { 204, 211, 255 }, { 7, 249, 255 },   { 178, 178, 178 }, { 0, 255, 255 }   }, // cyan
-				{ { 211, 197, 148 }, { 237, 216, 147 }, { 154, 220, 226 }, { 199, 199, 199 }, { 102, 255, 0 }   }, // lightgreen
-				{ { 35, 55, 125 },   { 1, 19, 81 },     { 71, 17, 15 },    { 34, 34, 34 },    { 143, 0, 255 }   }  // purple
+				{ { 128, 128, 128 }, { 127, 127, 129 }, { 128, 126, 130 }, { 128, 128, 128 }, { 127, 127, 127 } }, // gray
+				{ { 255, 255, 0 },   { 255, 232, 2 },   { 255, 242, 5 },   { 255, 214, 208 }, { 225, 225, 225 } }, // yellow
+				{ { 255, 0, 0 },     { 109, 86, 0 },    { 51, 40, 2 },     { 247, 5, 0 },     { 76, 76, 76 }    }, // red
+				{ { 0, 255, 255 },   { 145, 168, 255 }, { 204, 211, 255 }, { 7, 249, 255 },   { 178, 178, 178 } }, // cyan
+				{ { 102, 255, 0 },   { 211, 197, 148 }, { 237, 216, 147 }, { 154, 220, 226 }, { 199, 199, 199 } }, // lightgreen
+				{ { 143, 0, 255 },   { 35, 55, 125 },   { 1, 19, 81 },     { 71, 17, 15 },    { 34, 34, 34 }    }  // purple
 			};
 
 			Vars.SDColor = System.Drawing.Color.FromArgb
