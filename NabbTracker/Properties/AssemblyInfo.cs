a894f40fcf9a72169e232e328e8728244b37cc15
Exory <drago.enrico@hotmail.it>
2016-06-20 02:03:49 +0200

    [SDK]NabbTracker: 6.12.0.0
    - Updated for patch 6.12.

--- NabbTracker/Properties/AssemblyInfo.cs
+++ NabbTracker/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("6.10.1.6")]
-[assembly: AssemblyFileVersion("6.10.1.6")]
+[assembly: AssemblyVersion("6.12.0.0")]
+[assembly: AssemblyFileVersion("6.12.0.0")]
